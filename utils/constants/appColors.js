92724db3fffef6ab07cea1a18782dafdf67a1cd0
Fernando <fschmatz@hotmail.com>
2025-03-18 09:30:14 -0300

    v108b

--- utils/constants/appColors.js
+++ utils/constants/appColors.js
@@ -1,35 +1,19 @@
 export const AppColors = {
-  background: "#1C110C",
-  text: "#EBD1C6",
-  icons: "#EBD1C6",
-  primaryContainer: "#3E2C25",
-  secondaryContainer: "#2E2119",
+  background: "#1C1919",
+  text: "#E9D1C6",
+  icons: "#E9D1C6",
+  primaryContainer: "#292525",
+  secondaryContainer: "#262222",
 
   /* BTNs CALC */
   btnDeleteText: "#DDE8B3",
   btnDeleteBackground: "#424B23",
   btnConfirmText: "#D9E2FF",
-  btnConfirmBackground: "#27457F",
-  btnNumberBackground: "#2E2119",
+  btnConfirmBackground: "#3e5682",
+  btnNumberBackground: "#2A2525",
 
   androidRippleColor: { opacity: 0.6, backgroundColor: "#DADADA" },
   androidRippleEffect: { color: "rgba(0, 0, 0, 0.4)" },
 };
 
 export default AppColors;
-/*
- export const Colors = {
-    light: {
-    text: "black",
-    background: "white",
-    icons: "black",
-  }, 
-  dark: {
-    text: "#EBD1C6",
-    background: "#1C110C",
-    icons: "#EBD1C6",
-  },
-};
-
-export default Colors;
-*/
